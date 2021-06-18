f492abee57e2bfbdbc8af20a3d7078a3d2ae3721
kellykelly0808 <kellykelly0808@bitbucket.org>
2021-06-18 23:05:31 +0800

    frist

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+   res.render('index', { 
+     userName:req.session.username,
+     email:req.session.email
+    });
+});
+router.post('/',function(req,res){
+  req.session.username = req.body.username;
+  req.session.email = req.body.email;
+  res.redirect('/');
+})
+
+module.exports = router;
